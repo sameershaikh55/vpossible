dc2376156876bbd9edf688e51a65f055b49c2c0e
sameer-shaikh <sameermubeen4@gmail.com>
2021-08-17 06:02:05 +0500

    all done

--- src/components/ContactBody.jsx
+++ src/components/ContactBody.jsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { NavLink } from "react-router-dom";
 
 const ContactBody = ({ status, message, onSubmitted }) => {
+	const [succes, setSucces] = useState(false);
+
 	const {
 		register,
 		handleSubmit,
 		formState: { errors },
 		reset,
 	} = useForm();
+
+	let error;
+	if (Object.keys(errors).length) {
+		error = (
+			<p
+				style={{
+					color: "#FF5555",
+				}}
+				className="ms-lg-3 mb-0 f14"
+			>
+				Please complete all required fields in order to successfully submit
+				form.
+			</p>
+		);
+	} else {
+		if (succes)
+			error = (
+				<p
+					style={{
+						color: "#12D28D",
+					}}
+					className="ms-lg-3 mb-0 f14"
+				>
+					Your message has been submitted.
+				</p>
+			);
+	}
 
 	const onSubmit = (data) => {
 		const { firstname, surname, company, email, phone, messageT } = data;
@@ -26,13 +55,8 @@ const ContactBody = ({ status, message, onSubmitted }) => {
 
 		reset();
 
-		console.log(status, "status");
-		console.log(message, "message");
+		setSucces(true);
 	};
-
-	if (message !== null) {
-		var messageSplit = message.split("<");
-	}
 
 	return (
 		<div>
@@ -40,9 +64,9 @@ const ContactBody = ({ status, message, onSubmitted }) => {
 				<div className="page_container">
 					<div className="container-fluid">
 						<div className="row">
-							<div className="col-11 col-lg-12 mx-auto">
+							<div className="col-11 col-md-12 mx-auto">
 								<div className="row">
-									<div className="col-lg-4">
+									<div className="col-md-4 col-lg-4">
 										<h1 className="text-white f800">Get in-touch</h1>
 										<p className="textColor2">
 											Write to us regardling any sales or public relations
@@ -70,24 +94,37 @@ const ContactBody = ({ status, message, onSubmitted }) => {
 										</div>
 									</div>
 
-									<div className="d-block d-lg-none">
-										<hr className="mt-5 mb-0" />
+									<div className="d-block d-md-none">
+										<hr className="mt-5" />
 									</div>
 
 									<div className="col-1 px-0">
 										<div className="line ms-auto"></div>
 									</div>
-									<div className="form_container col-lg-7 ps-2 ps-lg-5 mt-5 mt-lg-0">
+									<div className="form_container col-md-7 ps-2 ps-md-5">
 										<form onSubmit={handleSubmit(onSubmit)}>
 											<div className="row">
-												<div className="col-12 col-sm-6 d-flex flex-column mtContact">
-													<label
-														className="text-white f14 mb6"
-														htmlFor="First Name*"
-													>
-														First Name*
-													</label>
-													<div className="position-relative w-100">
+												<div className="col-12 position-relative col-sm-6 d-flex flex-column mtContact">
+													<div className="d-flex align-items-center justify-content-between">
+														<label
+															className="text-white f14 mb6"
+															htmlFor="First Name*"
+														>
+															First Name*
+														</label>
+														<div className="redColor me-1">
+															{errors.firstname &&
+																errors.firstname.type === "required" &&
+																"Required"}
+															{/* {errors.firstname &&
+																errors.firstname.type === "maxLength" &&
+																"Maximum 10 Characters Allowed"}
+															{errors.firstname &&
+																errors.firstname.type === "pattern" &&
+																"please enter valid name"} */}
+														</div>
+													</div>
+													<div className="w-100">
 														<input
 															{...register("firstname", {
 																required: true,
@@ -101,12 +138,25 @@ const ContactBody = ({ status, message, onSubmitted }) => {
 													</div>
 												</div>
 												<div className="col-12 col-sm-6 d-flex flex-column mtContact">
-													<label
-														className="text-white f14 mb6"
-														htmlFor="First Name*"
-													>
-														Surname*
-													</label>
+													<div className="d-flex align-items-center justify-content-between">
+														<label
+															className="text-white f14 mb6"
+															htmlFor="First Name*"
+														>
+															Surname*
+														</label>
+														<div className="redColor">
+															{errors.surname &&
+																errors.surname.type === "required" &&
+																"Required"}
+															{/* {errors.surname &&
+																errors.surname.type === "maxLength" &&
+																"Maximum 10 Characters Allowed"} */}
+															{/* {errors.surname &&
+																errors.surname.type === "pattern" &&
+																"please enter valid name"} */}
+														</div>
+													</div>
 													<div className="position-relative w-100">
 														<input
 															className="py-1 w-100"
@@ -121,13 +171,24 @@ const ContactBody = ({ status, message, onSubmitted }) => {
 													</div>
 												</div>
 												<div className="col-12 d-flex flex-column mtContact">
-													<label
-														className="text-white f14 mb6"
-														htmlFor="First Name*"
-													>
-														Email Address*
-													</label>
-													<div className="position-relative w-100">
+													<div className="d-flex align-items-center justify-content-between">
+														<label
+															className="text-white f14 mb6"
+															htmlFor="First Name*"
+														>
+															Email Address*
+														</label>
+														<div className="redColor">
+															{errors.email &&
+																errors.email.type === "required" &&
+																"Required"}
+															{errors.email &&
+																errors.email.type === "pattern" &&
+																"Invalid Email Address"}
+														</div>
+													</div>
+
+													<div className="w-100">
 														<input
 															className="py-1 w-100"
 															type="email"
@@ -147,15 +208,7 @@ const ContactBody = ({ status, message, onSubmitted }) => {
 														Phone*
 													</label>
 													<div className="position-relative w-100">
-														<input
-															className="py-1 w-100"
-															type="number"
-															{...register("phone", {
-																required: true,
-																minLength: 8,
-																maxLength: 11,
-															})}
-														/>
+														<input className="py-1 w-100" type="number" />
 													</div>
 												</div>
 												<div className="col-12 col-md-6 d-flex flex-column mtContact">
@@ -166,48 +219,42 @@ const ContactBody = ({ status, message, onSubmitted }) => {
 														Company*
 													</label>
 													<div className="position-relative w-100">
-														<input
-															className="py-1 w-100"
-															type="text"
-															{...register("company", {
-																required: true,
-																maxLength: 15,
-															})}
-														/>
+														<input className="py-1 w-100" type="text" />
 													</div>
 												</div>
-												<div className="col-12 d-flex flex-column mtContact">
-													<label
-														className="text-white f14 mb6"
-														htmlFor="First Name*"
-													>
-														Message*
-													</label>
-													<textarea {...register("messageT")} />
+												<div className="col-12 position-relative d-flex flex-column mtContact">
+													<div className="d-flex justify-content-between align-items-center">
+														<label
+															className="text-white f14 mb6"
+															htmlFor="First Name*"
+														>
+															Message*
+														</label>
+														<div className="redColor">
+															{errors.messageT &&
+																errors.messageT.type === "required" &&
+																"Required"}
+														</div>
+													</div>
+
+													<textarea
+														{...register("messageT", {
+															required: true,
+														})}
+													/>
 												</div>
 											</div>
 
 											<div className="mt-4 w-100 d-flex flex-column flex-lg-row align-items-center">
-												<div className="d-inline-block">
+												<div className="btn_cont_submit">
 													<button
 														type="submit"
-														className="f600 border-0 px-4 py-2 f14 w-100"
+														className="f600 border-0 px-4 py-2 f14"
 													>
 														Submit
 													</button>
 												</div>
-												<p
-													style={{
-														color: "#FF5555",
-													}}
-													className="ms-3 mb-0 f14"
-												></p>
-												<p
-													style={{
-														color: "#12D28D",
-													}}
-													className="ms-3 mb-0 f14"
-												></p>
+												<div className="mt-2 mt-lg-0">{error}</div>
 											</div>
 										</form>
 									</div>
